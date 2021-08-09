c131652ddce55c7064068c31b80213e41fe8c268
HYnam <namhy2006@gmail.com>
2021-08-09 22:10:40 +0800

    Create Constructor Cargo

--- src/portsim/cargo/Cargo.java
+++ src/portsim/cargo/Cargo.java
@@ -1,6 +1,19 @@
 package portsim.cargo;
 
 
-public abstract class Cargo {
+public abstract class Cargo
+{
+    public Cargo(int id, String destination)
+    // Creates a new Cargo with the given ID and destination port.
+    {
+        int Cargo_id = id;
+        String dest_port = destination;
+
+        if(Cargo_id < 0)
+        {
+            throw new IllegalArgumentException ("id out of range");
+        }
+    }
+
 
 }
