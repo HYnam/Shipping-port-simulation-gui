c315a39600d7cdbd4256bf1bf2707805cf7c77d7
HYnam <namhy2006@gmail.com>
2021-08-09 16:43:04 +0800

    BulkCargo constructor finish getTonnage method define

--- src/portsim/cargo/BulkCargo.java
+++ src/portsim/cargo/BulkCargo.java
@@ -1,5 +1,17 @@
 package portsim.cargo;
 
-public class BulkCargo {
+public class BulkCargo
+{
+     // Bulk cargo is commodity cargo that is transported unpacked in large quantities.
+    public BulkCargo(int id, String destination, int tonnage, BulkCargoType type){
+        // Constructor: Creates a new Bulk Cargo with the given ID, destination, tonnage and type.
+        if (id < 0 || tonnage < 0){
+            throw new IllegalArgumentException ("id or tonnage out of range");
+        }
+    }
 
+    public int getTonnage(){
+        // Method: Returns the tonnage of this bulk cargo.
+        return tonnage;
+    }
 }
