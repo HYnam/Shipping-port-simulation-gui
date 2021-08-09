4de0c4ee4b7f770cf5f5ca17b54d2e1e6c0c6227
HYnam <namhy2006@gmail.com>
2021-08-09 18:38:20 +0800

    Initialize variables in constructor BulkCargo

--- src/portsim/cargo/BulkCargo.java
+++ src/portsim/cargo/BulkCargo.java
@@ -1,17 +1,27 @@
 package portsim.cargo;
 
 public class BulkCargo
+// Bulk cargo is commodity cargo that is transported unpacked in large quantities.
 {
-     // Bulk cargo is commodity cargo that is transported unpacked in large quantities.
-    public BulkCargo(int id, String destination, int tonnage, BulkCargoType type){
-        // Constructor: Creates a new Bulk Cargo with the given ID, destination, tonnage and type.
-        if (id < 0 || tonnage < 0){
+
+    public BulkCargo(int id, String destination, int tonnage, BulkCargoType type)
+    // Constructor: Creates a new Bulk Cargo with the given ID, destination, tonnage and type.
+    {
+        // Initialise variables
+        int cargo_id = id;
+        String des_port = destination;
+        int weight_cargo = tonnage;
+        BulkCargoType type_cargo = type;
+
+        if (id < 0 || tonnage < 0)
+        {
             throw new IllegalArgumentException ("id or tonnage out of range");
         }
     }
 
-    public int getTonnage(){
-        // Method: Returns the tonnage of this bulk cargo.
-        return tonnage;
+    public int getTonnage()
+    // Method: Returns the tonnage of this bulk cargo.
+    {
+        return weight_cargo;
     }
 }
