d19dbba53dd35b122c06aed02b0fa644e8b1bb61
HYnam <namhy2006@gmail.com>
2021-08-10 15:34:40 +0800

    Itialize variables, toString method

--- src/portsim/cargo/BulkCargo.java
+++ src/portsim/cargo/BulkCargo.java
@@ -3,15 +3,19 @@ package portsim.cargo;
 public class BulkCargo
 // Bulk cargo is commodity cargo that is transported unpacked in large quantities.
 {
+    public int cargo_id;
+    public String dest_port;
+    public int weight;
+    public BulkCargoType type_of_cargo;
 
     public BulkCargo(int id, String destination, int tonnage, BulkCargoType type)
     // Constructor: Creates a new Bulk Cargo with the given ID, destination, tonnage and type.
     {
         // Initialise variables
-        int cargo_id = id;
-        String des_port = destination;
-        int weight_cargo = tonnage;
-        BulkCargoType type_cargo = type;
+        cargo_id = id;
+        dest_port = destination;
+        weight = tonnage;
+        type_of_cargo = type;
 
         if (id < 0 || tonnage < 0)
         {
@@ -22,18 +26,19 @@ public class BulkCargo
     public int getTonnage()
     // Method: Returns the tonnage of this bulk cargo.
     {
-        return weight_cargo;
+        return cargo_id;
     }
 
     public BulkCargoType getType()
     // Method: Returns the type of this bulk cargo.
     {
-        return type_cargo;
+        return type_of_cargo;
     }
 
+    @Override
     public String toString()
     // Method: Returns the human-readable string representation of this BulkCargo.
     {
-
+        return super.toString() + "BulkCargo" + cargo_id + "to" + dest_port + '[' + type_of_cargo + '-' + weight + ']';
     }
 }
