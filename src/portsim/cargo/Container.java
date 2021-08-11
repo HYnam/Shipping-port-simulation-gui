d77402d0fddf372894a3d6debdb7e29bf3e4792b
HYnam <namhy2006@gmail.com>
2021-08-11 14:22:24 +0800

    method getType and toString() in Container.java

--- src/portsim/cargo/Container.java
+++ src/portsim/cargo/Container.java
@@ -1,5 +1,31 @@
 package portsim.cargo;
 
-public class Container {
+public class Container
+{
+    public int cargo_id;
+    public String dest_port;
+    public ContainerType type;
 
+    public Container(int id, String destination, ContainerType type)
+    {
+        cargo_id = id;
+        dest_port = destination;
+        this.type = type;
+
+        if (id < 0)
+        {
+            throw new IllegalArgumentException ("id out of range");
+        }
+    }
+
+    public ContainerType getType()
+    {
+        return type;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + "Container" + cargo_id + "to" + dest_port + "[" + type + "]";
+    }
 }
