d0e04593889654d8a899f33dcbfc53e0ca628ddf
HYnam <namhy2006@gmail.com>
2021-08-12 11:13:58 +0800

    Constructor of CargoMovement.java

--- src/portsim/movement/CargoMovement.java
+++ src/portsim/movement/CargoMovement.java
@@ -1,5 +1,25 @@
 package portsim.movement;
 
-public class CargoMovement {
+import portsim.cargo.Cargo;
+import java.util.List;
 
+public class CargoMovement extends Movement
+// The movement of cargo coming into or out of the port.
+{
+    long timeMovement;
+    MovementDirection directionOfMovement;
+    List<Cargo> cargoToMove;
+
+    public CargoMovement(long time, MovementDirection direction, List<Cargo> cargo)
+    // Creates a new cargo movement with the given action time and direction to be undertaken with the given cargo.
+    {
+        this.timeMovement = time;
+        this.directionOfMovement = direction;
+        this.cargoToMove = cargo;
+
+        if(time < 0)
+        {
+            throw new IllegalArgumentException ("Must have time");
+        }
+    }
 }
