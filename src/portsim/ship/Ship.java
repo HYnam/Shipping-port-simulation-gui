d93459cb7002ac17684f0a6c87d1f5703aaf9fa5
HYnam <namhy2006@gmail.com>
2021-08-13 17:00:57 +0800

    Ship class constructor

--- src/portsim/ship/Ship.java
+++ src/portsim/ship/Ship.java
@@ -1,5 +1,23 @@
 package portsim.ship;
 
-public abstract class Ship {
+public abstract class Ship extends Object
+/*
+    Represents a ship whose movement is managed by the system
+    Ships store various types of cargo which can be loaded and unloaded at a port
+ */
+{
+    long Identifier;
+    String NameOfShip;
+    String PortOfOrigin;
+    NauticalFlag FlagShipFlying;
+    public Ship(long imoNumber, String name, String originFlag, NauticalFlag flag)
+    // Creates a new ship with the given IMO number, name, origin port flag and nautical flag
+    {
+        this.Identifier = imoNumber;
+        this.NameOfShip = name;
+        this.PortOfOrigin = originFlag;
+        this.FlagShipFlying = flag;
 
+        if(imoNumber < 0 || imoNumber.length())
+    }
 }
