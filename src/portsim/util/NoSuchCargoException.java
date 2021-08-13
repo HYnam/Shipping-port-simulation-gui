85dd020c4dbc1336968d8535139a3d0be86b0821
HYnam <namhy2006@gmail.com>
2021-08-13 16:48:44 +0800

    try catch throw exception

--- src/portsim/util/NoSuchCargoException.java
+++ src/portsim/util/NoSuchCargoException.java
@@ -12,8 +12,8 @@ public class NoSuchCargoException extends Exception
     public NoSuchCargoException(String message)
     // Constructs a NoSuchCargoException that contains a helpful detail message explaining why the exception occurred
     {
-        try{
-            throw new NoSuchCargoException();
+        try {
+            throw new NoSuchCargoException(message);
         } catch (NoSuchCargoException e) {
             e.printStackTrace();
         }
