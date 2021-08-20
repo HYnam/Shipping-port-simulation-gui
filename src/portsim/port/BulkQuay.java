c502d3a0d19024482769f29369dc7f80bcbc07ea
HYnam <namhy2006@gmail.com>
2021-08-20 11:57:40 +0800

    Bulk.java done with no error

--- src/portsim/port/BulkQuay.java
+++ src/portsim/port/BulkQuay.java
@@ -1,6 +1,37 @@
 package portsim.port;
 
 public class BulkQuay extends Quay
+// A Bulk Quay is a type of quay specifically designed for the unloading of Bulk Carrier vessels.
 {
+    int MaxTonnageCanHandle;
 
+    public BulkQuay(int id, int maxTonnage)
+    // Creates a new Bulk Quay with the given ID and max tonnage.
+    {
+        super(id);
+        this.MaxTonnageCanHandle = maxTonnage;
+
+        if(id < 0 || maxTonnage < 0)
+        {
+            throw new IllegalArgumentException ("ID or maxTonnage must be greater than zero");
+        }
+    }
+
+    public int getMaxTonnage()
+    // Returns the maximum number of tonnes of cargo this quay can handle
+    {
+        return this.MaxTonnageCanHandle;
+    }
+
+    @Override
+    public String toString()
+    /*
+    Returns the human-readable string representation of this BulkQuay.
+        The format of the string to return is
+
+        BulkQuay id [Ship: imoNumber] - maxTonnage
+     */
+    {
+        return super.toString() + "BulkQuay " + this.QuayId + "[Ship: " + this.getShip() + "] - " + this.getMaxTonnage();
+    }
 }
