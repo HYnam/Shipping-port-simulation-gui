bbe4bbfcb55779ef4137ec320e03601a8860479b
Alvin Leung <alvinleung198123@gmail.com>
2021-09-07 17:24:05 +0800

    Added several test cases

--- test/portsim/ship/BulkCarrierTest.java
+++ test/portsim/ship/BulkCarrierTest.java
@@ -3,6 +3,11 @@ package portsim.ship;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import portsim.cargo.BulkCargo;
+import portsim.cargo.BulkCargoType;
+import portsim.port.BulkQuay;
+import portsim.port.ContainerQuay;
+import portsim.util.NoSuchCargoException;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +17,10 @@ public class BulkCarrierTest {
     private BulkCarrier bulkCarrier2;
     private BulkCarrier bulkCarrier3;
 
+    private BulkQuay bulkQuay;
+    private ContainerQuay containerQuay;
+
+    private BulkCargo bulkCargo;
     @Before
     public void setUp() {
         bulkCarrier1 = new BulkCarrier(1234567, "Peter", "Hong Kong", NauticalFlag.BRAVO, 2000);
@@ -19,8 +28,79 @@ public class BulkCarrierTest {
         bulkCarrier3 = new BulkCarrier(5555678, "Jackson", "New York", NauticalFlag.NOVEMBER, 5000);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void testCanDockWithCorrectQuay() {
+        bulkQuay = new BulkQuay(300, 10000);
+        boolean result = bulkCarrier1.canDock(bulkQuay);
+        boolean expected = true;
+        assertTrue(result == expected);
+    }
+
+    @Test
+    public void testCanDockWithIncorrectQuay() {
+        containerQuay = new ContainerQuay(301, 10);
+        boolean result = bulkCarrier1.canDock(containerQuay);
+        boolean expected = false;
+        assertTrue(result == expected);
+    }
+
+    @Test
+    public void testCanLoadWithCorrectCargo() {
+        bulkCargo = new BulkCargo(231003, "Hong Kong", 500, BulkCargoType.OIL);
+        boolean result = bulkCarrier1.canLoad(bulkCargo);
+        boolean expected = true;
+        assertTrue(result == expected);
+    }
+
+    @Test
+    public void testCanLoadCargoWithIncorrectDestination() {
+        bulkCargo = new BulkCargo(231003, "London", 500, BulkCargoType.OIL);
+        boolean result = bulkCarrier1.canLoad(bulkCargo);
+        boolean expected = false;
+        assertTrue(result == expected);
+    }
+
+    @Test
+    public void testCanLoadCargoWithIncorrectTonnage() {
+        bulkCargo = new BulkCargo(231003, "Hong Kong", 50000, BulkCargoType.OIL);
+        boolean result = bulkCarrier1.canLoad(bulkCargo);
+        boolean expected = false;
+        assertTrue(result == expected);
+    }
+
+    @Test
+    public void testGetCargo() {
+        bulkCargo = new BulkCargo(231003, "Hong Kong", 500, BulkCargoType.OIL);
+        bulkCarrier1.loadCargo(bulkCargo);
+        BulkCargo result = bulkCarrier1.getCargo();
+        BulkCargo expected = bulkCargo;
+        assertEquals("Cannot get the cargo from bulkCarrier", result, expected);
+    }
+
+    @Test
+    public void testUnloadCargo() throws NoSuchCargoException {
+        bulkCargo = new BulkCargo(231003, "Hong Kong", 500, BulkCargoType.OIL);
+        bulkCarrier1.loadCargo(bulkCargo);
+        BulkCargo result;
+        BulkCargo expected;
+
+        result = bulkCarrier1.unloadCargo();
+        expected = bulkCargo;
+        assertEquals("The return form unloadCargo is incorrect", result, expected);
+
+        result = bulkCarrier1.getCargo();
+        expected = null;
+        assertEquals("The cargo in bulkCarrier1 is not null after unloadCargo()", result, expected);
+    }
+
+    @Test(expected = NoSuchCargoException.class)
+    public void testUnloadCargoException() throws NoSuchCargoException {
+        if(bulkCarrier1.getCargo() == null) {
+            bulkCarrier1.unloadCargo();
+        } else {
+            bulkCarrier1.unloadCargo();
+            bulkCarrier1.unloadCargo();
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -49,23 +129,31 @@ public class BulkCarrierTest {
     }
 
     @Test
-    public void testToStringBulkCarrier1(){
+    public void testToStringBulkCarrier1() {
+        bulkCargo = new BulkCargo(231003, "Hong Kong", 500, BulkCargoType.MINERALS);
+        bulkCarrier1.loadCargo(bulkCargo);
         String expected = "BulkCarrier Peter from Hong Kong [BRAVO] carrying MINERALS";
-
         assertEquals("The message toString does not match", expected, bulkCarrier1.toString());
     }
 
     @Test
-    public void testToStringBulkCarrier2(){
+    public void testToStringBulkCarrier2() {
+        bulkCargo = new BulkCargo(231003, "London", 500, BulkCargoType.OIL);
+        bulkCarrier2.loadCargo(bulkCargo);
         String expected = "BulkCarrier Johnson from London [HOTEL] carrying OIL";
-
         assertEquals("The message toString does not match", expected, bulkCarrier2.toString());
     }
 
     @Test
-    public void testToStringBulkCarrier3(){
+    public void testToStringBulkCarrier3() {
+        bulkCargo = new BulkCargo(231003, "New York", 500, BulkCargoType.COAL);
+        bulkCarrier3.loadCargo(bulkCargo);
         String expected = "BulkCarrier Jackson from New York [NOVEMBER] carrying COAL";
-
         assertEquals("The message toString does not match", expected, bulkCarrier3.toString());
     }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
 }
