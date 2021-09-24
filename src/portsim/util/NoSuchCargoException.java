b1e4be91f86838c6fd5846979faa5fbb8c3f8cd7
HYnam <namhy2006@gmail.com>
2021-09-24 09:48:49 +0800

    Constructors done

--- src/portsim/util/NoSuchCargoException.java
+++ src/portsim/util/NoSuchCargoException.java
@@ -35,4 +35,23 @@ public class NoSuchCargoException extends Exception {
         super(message);
     }
 
+    /** Constructs a NoSuchCargoException that stores the underlying cause of the exception.
+     * @param cause throwable that caused this exception
+     * @see Exception(Throwable)
+     * */
+    public NoSuchCargoException(Throwable cause){
+        super(cause);
+    }
+
+    /** Constructs a NoSuchCargoException that contains a helpful detail message explaining
+     * why the exception occurred and the underlying cause of the exception.
+     *
+     * @param message detail message
+     * @param cause throwable that caused this exception
+     * @see Exception(String, Throwable)
+     * */
+    public NoSuchCargoException(String message, Throwable cause){
+        super(message, cause);
+    }
+
 }
