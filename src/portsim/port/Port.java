5b2cdbec39be7c448fff39912068c739fe624c1a
HYnam <namhy2006@gmail.com>
2021-09-27 23:00:20 +0800

    Constructors, getTime(), getShipQueue(), getMovements(), getEvaluators() done. Others with format/Javadoc

--- src/portsim/port/Port.java
+++ src/portsim/port/Port.java
@@ -1,8 +1,14 @@
 package portsim.port;
 
 import portsim.cargo.Cargo;
+import portsim.evaluators.StatisticsEvaluator;
 import portsim.movement.Movement;
+import portsim.util.BadEncodingException;
+import portsim.util.Encodable;
+import portsim.util.Tickable;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.util.*;
 
 /**
@@ -13,7 +19,7 @@ import java.util.*;
  *
  * @ass1_partial
  */
-public class Port {
+public class Port implements Tickable, Encodable {
 
     /**
      * The name of this port used for identification
@@ -27,6 +33,18 @@ public class Port {
      * The cargo currently stored at the port at warehouses. Cargo unloaded from trucks / ships
      */
     private List<Cargo> storedCargo;
+
+    /** The time since the simulation was started */
+    private long timeSimulationStarted;
+
+    /** Queue of ships waiting to be docked at this port */
+    private ShipQueue shipQueue;
+
+    /** To store movements ordered by the time of the movement */
+    private PriorityQueue<Movement> priorityQueue;
+
+    /** Store the statistics Evaluator of the port's operation */
+    private List<StatisticsEvaluator> statisticsEvaluatorList;
 
     /**
      * Creates a new port with the given name.
@@ -42,10 +60,96 @@ public class Port {
      * @param name name of the port
      * @ass1_partial
      */
-    public Port(String name) throws IllegalArgumentException {
+    public Port(String name) {
         this.name = name;
+
         this.quays = new ArrayList<Quay>();
         this.storedCargo = new ArrayList<Cargo>();
+        this.timeSimulationStarted = 0;
+        this.shipQueue = null;
+        this.priorityQueue = null;  // Not sure
+        this.statisticsEvaluatorList = new ArrayList<StatisticsEvaluator>();
+    }
+
+    /** Creates a new port with the given name, time elapsed, ship queue, quays and stored
+     * cargo
+     *
+     * The list of statistics evaluators should be initialised as an empty list.
+     *
+     * A PriorityQueue should be initialised to store movements ordered by the time of the
+     * movement
+     *
+     * @param name name of the port
+     * @param time number of minutes since simulation started
+     * @param shipQueue ships waiting to enter the port
+     * @param quays the port's quays
+     * @param storedCargo the cargo stored at the port
+     *
+     * @throws IllegalArgumentException if time < 0
+     * */
+    public Port(String name, long time, ShipQueue shipQueue,
+                List<Quay> quays, List<Cargo> storedCargo)
+        throws IllegalArgumentException{
+        if (time < 0){
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.timeSimulationStarted = time;
+        this.shipQueue = shipQueue;
+        this.quays = quays;
+        this.storedCargo = storedCargo;
+
+        this.statisticsEvaluatorList = new ArrayList<StatisticsEvaluator>();
+        this.priorityQueue = null;  // Not sure
+    }
+
+    /** Adds a movement to the PriorityQueue of movements.
+     *
+     * If the given movement's action time is less than the current number of minutes
+     * elapsed than an IllegalArgumentException should be thrown.
+     *
+     * @param movement movement to add
+     * @throws IllegalArgumentException
+     * */
+    public void addMovement(Movement movement) throws
+            IllegalArgumentException{
+
+    }
+
+    /** Processes a movement.
+     * The action taken depends on the type of movement to be processed.
+     * If the movement is a ShipMovement:
+     * <ul>
+     *     <li>If the movement direction is INBOUND then the ship should be added to the ship
+     *     queue</li>
+     *
+     *     <li>If the movement direction is OUTBOUND then any cargo stored in the port whose
+     *     destination is the ship's origin port should be added to the ship according to
+     *     Ship.canLoad(Cargo). Next, the ship should be removed from the quay it is
+     *     currently docked in (if any).</li>
+     * </ul>
+     * If the movement is a CargoMovement:
+     * <ul>
+     *     <li>If the movement direction is INBOUND then all of the cargo that is being moved
+     *     should be added to the port's stored cargo.</li>
+     *
+     *     <li>If the movement direction is OUTBOUND then all cargo with the given IDs should
+     *     be removed from the port's stored cargo.</li>
+     * </ul>
+     * Finally, the movement should be forwarded onto each statistics evaluator stored by the
+     * port by calling StatisticsEvaluator.onProcessMovement(Movement).
+     *
+     * @param movement movement to execute*/
+    public void processMovement(Movement movement){
+
+    }
+
+    /** Adds the given statistics evaluator to the port's list of evaluators.
+     * If the port already has an evaluator of that type, no action should be taken.
+     * @param eval statistics evaluator to add to the port
+     * */
+    public void addStatisticsEvaluator(StatisticsEvaluator eval){
+
     }
 
     /**
@@ -56,6 +160,13 @@ public class Port {
      */
     public String getName() {
         return name;
+    }
+
+    /** Returns the time since simulation started
+     * @return time in minutes
+     * */
+    public long getTime(){
+        return timeSimulationStarted;
     }
 
     /**
@@ -85,6 +196,27 @@ public class Port {
         return new ArrayList<>(this.storedCargo);
     }
 
+    /** Returns the queue of ships waiting to be docked at this port.
+     * @return port's queue of ships
+     * */
+    public ShipQueue getShipQueue(){
+        return shipQueue;
+    }
+
+    public PriorityQueue<Movement> getMovements(){
+        return priorityQueue;
+    }
+
+    /** Returns the list of evaluators at the port.
+     * Adding or removing elements from the returned list should not affect the original list.
+     * @return the ports evaluators
+     * */
+    public List<StatisticsEvaluator> getEvaluators(){
+        List<StatisticsEvaluator> result = new ArrayList<>();   // Add new List
+        result.addAll(statisticsEvaluatorList); //Copy to new list
+        return result;
+    }
+
     /**
      * Adds a quay to the ports control.
      *
@@ -93,6 +225,20 @@ public class Port {
      */
     public void addQuay(Quay quay) {
         this.quays.add(quay);
+    }
+
+
+    public void elapseOneMinute(){
+
+    }
+
+    public String encode(){
+
+    }
+
+    public static Port initialisePort(Reader reader) throws
+            IOException, BadEncodingException{
+
     }
 
 }
