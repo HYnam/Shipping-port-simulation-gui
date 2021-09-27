d4028db29f4100f1d24def40453a3dcb81f9e1a6
HYnam <namhy2006@gmail.com>
2021-09-27 21:44:03 +0800

    Class done

--- src/portsim/evaluators/StatisticsEvaluator.java
+++ src/portsim/evaluators/StatisticsEvaluator.java
@@ -1,5 +1,40 @@
 package portsim.evaluators;
 
-public abstract class StatisticsEvaluator {
+import portsim.movement.Movement;
+import portsim.util.Tickable;
 
+import java.time.Instant;
+
+/** A base class representing an object that gathers and reports data on various aspects of the
+ * port's operation
+ * */
+public abstract class StatisticsEvaluator implements Tickable {
+
+    long timeEvaluatorCreated;
+
+    /** Creates a statistics evaluator and initialises the time since the evaluator was created to
+     * zero
+     * */
+    public StatisticsEvaluator(){
+        this.timeEvaluatorCreated = 0;
+    }
+
+    public long getTime(){
+        return this.timeEvaluatorCreated;
+    }
+
+    /** Read a movement to update the relevant evaluator data.
+     * This method is called by the Port.processMovement(Movement) method.
+     * @param movement movement to read
+     * */
+    public abstract void onProcessMovement(Movement movement);
+
+    /** Simulate a minute passing. The time since the evaluator was created should be
+     * increment by one
+     * */
+    public void elapseOneMinute(){
+        for (Instant.now().plusSeconds(60);;){  //Count 1 minute
+            this.timeEvaluatorCreated ++;
+        }
+    }
 }
