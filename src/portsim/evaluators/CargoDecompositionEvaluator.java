945803b67a28eb15c59760b40f81a1720caa3812
HYnam <namhy2006@gmail.com>
2021-09-30 22:05:42 +0800

    constructor initialize, getContainerDistribution(), getBulkCargoDistribution(), getCargoDistribution() done

--- src/portsim/evaluators/CargoDecompositionEvaluator.java
+++ src/portsim/evaluators/CargoDecompositionEvaluator.java
@@ -10,49 +10,51 @@ import java.util.Map;
 /** Collects data on what types of cargo are passing through the port. Gathers data on all
  * derivatives of the cargo class
  *
- * The data gathered is a count of how many times each type of cargo has entered the port. Thes
+ * The data gathered is a count of how many times each type of cargo has entered the port. This
  * includes a count of how many times the port has received "BulkCargo" or "Container" class
  * cargo. As well as a count of how many times the port has seen each cargo subclass type
  * */
 public class CargoDecompositionEvaluator extends StatisticsEvaluator {
 
     /** To store the number of distribution on cargo types entered the port */
-    private Map<String, Integer> cargoDistribution = new HashMap<>();
+    private Map<String, Integer> cargoDistribution;
 
     /** To store the number of distribution on BulkCargo type entered the port */
-    private Map<BulkCargo,Integer> bulkCargoDistribution = new HashMap<>();
+    private Map<BulkCargo,Integer> bulkCargoDistribution;
 
     /** To store the number of distribution on Container type entered the port */
-    private Map<Container, Integer> containerDistribution = new HashMap<>();
+    private Map<Container, Integer> containerDistribution;
 
     /** Constructs a new CargoDecompositionEvaluator */
     public CargoDecompositionEvaluator(){
-
+        this.cargoDistribution = new HashMap<>();
+        this.bulkCargoDistribution = new HashMap<>();
+        this.containerDistribution = new HashMap<>();
     }
 
     /** Return the distribution of which cargo types that have entered the port
      * @return cargo distribution map
      * */
     public Map<String, Integer> getCargoDistribution(){
-
+        return this.cargoDistribution;
     }
 
     /** Return the distribution of bulk cargo types that have entered the port
      * @return bulk cargo distribution map
      * */
     public Map<BulkCargo, Integer> getBulkCargoDistribution(){
-
+        return this.bulkCargoDistribution;
     }
 
     /** Return the distribution of container cargo types that have entered the port
      * @return container distribution map
      * */
     public Map<Container, Integer> getContainerDistribution(){
-
+        return this.containerDistribution;
     }
 
     /** Updates the internal distributions of cargo types using the given movement
-     * If the movement is not an {@code INBOUND} movement, this method reutrn immediately
+     * If the movement is not an {@code INBOUND} movement, this method return immediately
      * without taking any action
      *
      * If the movement is an {@code INBOUND} movement, do the following:
@@ -80,6 +82,6 @@ public class CargoDecompositionEvaluator extends StatisticsEvaluator {
      * @param movement movement to read
      * */
     public void onProcessMovement(Movement movement){
-
+        if ()
     }
 }
